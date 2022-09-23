60aa1ba59aaa14d3199e6501db8d21c36e4331d7
Philip <philipdeucher07@gmail.com>
2022-09-23 15:03:12 -0400

    rat

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/SampleMecanumDrive.java
@@ -29,6 +29,8 @@ import com.qualcomm.robotcore.hardware.configuration.typecontainers.MotorConfigu
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceBuilder;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequenceRunner;
+import org.firstinspires.ftc.teamcode.util.AxisDirection;
+import org.firstinspires.ftc.teamcode.util.BNO055IMUUtil;
 import org.firstinspires.ftc.teamcode.util.LynxModuleUtil;
 
 import java.util.ArrayList;
@@ -114,12 +116,12 @@ public class SampleMecanumDrive extends MecanumDrive {
         // and the placement of the dot/orientation from https://docs.revrobotics.com/rev-control-system/control-system-overview/dimensions#imu-location
         //
         // For example, if +Y in this diagram faces downwards, you would use AxisDirection.NEG_Y.
-        // BNO055IMUUtil.remapZAxis(imu, AxisDirection.NEG_Y);
+         BNO055IMUUtil.remapZAxis(imu, AxisDirection.POS_Z);
 
-        leftfront = hardwareMap.get(DcMotorEx.class, "leftFront");
-        leftback = hardwareMap.get(DcMotorEx.class, "leftRear");
-        rightback = hardwareMap.get(DcMotorEx.class, "rightRear");
-        rightfront = hardwareMap.get(DcMotorEx.class, "rightFront");
+        leftfront = hardwareMap.get(DcMotorEx.class, "leftfront");
+        leftback = hardwareMap.get(DcMotorEx.class, "leftback");
+        rightback = hardwareMap.get(DcMotorEx.class, "rightback");
+        rightfront = hardwareMap.get(DcMotorEx.class, "rightfront");
 
         motors = Arrays.asList(leftfront, leftback, rightback, rightfront);
 
